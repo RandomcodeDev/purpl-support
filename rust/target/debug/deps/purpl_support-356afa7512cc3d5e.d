6dc2bbbb8aec42335604c75a090af22b88e21491
MobSlicer152 <brambleclaw1414@gmail.com>
2024-03-01 10:36:22 -0500

    Remove Rust binaries

--- rust/target/debug/deps/purpl_support-356afa7512cc3d5e.d
+++
@@ -1,7 +0,0 @@
-E:\mobslicer152\Desktop\purpl-engine-private\purpl-engine\support\rust\target\debug\deps\libpurpl_support-356afa7512cc3d5e.rmeta: src\lib.rs
-
-E:\mobslicer152\Desktop\purpl-engine-private\purpl-engine\support\rust\target\debug\deps\libpurpl_support-356afa7512cc3d5e.rlib: src\lib.rs
-
-E:\mobslicer152\Desktop\purpl-engine-private\purpl-engine\support\rust\target\debug\deps\purpl_support-356afa7512cc3d5e.d: src\lib.rs
-
-src\lib.rs:
--- rust/target/debug/libpurpl_support.d
+++
@@ -1,1 +0,0 @@
-E:\mobslicer152\Desktop\purpl-engine-private\purpl-engine\support\rust\target\debug\libpurpl_support.rlib: E:\mobslicer152\Desktop\purpl-engine-private\purpl-engine\support\rust\src\lib.rs
